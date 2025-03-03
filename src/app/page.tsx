ba5916c41a116edfeeb89dd2d83b66cc6efbcbf6
강지호 <gangjiho@gangjihoui-MacBookPro.local>
2025-03-03 16:24:49 +0900

    remove data in rect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
+import { pixelWidth, screenHeight, screenWidth } from "@/constant/constant";
 import { useDrag } from "@/hooks/Drag";
-
-import Image from "next/image";
-import { useEffect, useMemo, useRef } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 export const randomGenerate = (): number[][] =>{
   const arr: number[][] = [];
@@ -19,35 +18,99 @@ export const randomGenerate = (): number[][] =>{
   return arr;
 }
 
-function draw(ctx: any, line: number[][])
-{
-  for (let i = 0; i < 10; i++)
-    {
-      for (let j = 0; j < 17; j++)
-      {
-        ctx.fillStyle = "black";
-        ctx.fillText(line[i][j].toString(), j * 100, i * 100); 
-      }
-    }
-}
-
-
 export default function Home() {
   const [onDragStart, onDragDuration, onDragEnd, position] = useDrag()
   const ref = useRef<HTMLCanvasElement>(null);
   const line = useMemo(() => randomGenerate(), []);
-  console.log(line);
+  const [numLines, setLines] = useState(line);
+  const [value, setValue] = useState(0);
+
+
+  const draw = useCallback((ctx: CanvasRenderingContext2D, line: number[][]) => {
+    for (let i = 0; i < 10; i++) {
+      for (let j = 0; j < 17; j++) {
+        if (line[i][j] === 0)
+          continue ;
+        
+        ctx.fillStyle = "black";
+        ctx.font = "40px Arial";
+        ctx.fillText(line[i][j].toString(), pixelWidth + j * pixelWidth, pixelWidth + i * pixelWidth); 
+      }
+    }
+  }, []);
+  const updateLine = (row: [number, number], col: [number, number]) => {
+    const newLines = [...numLines];
+    
+    for (let i = 0; i <= col[1]; i++) {
+      for (let j = 0; j <= row[1]; j++) {
+        newLines[col[0] + i][row[0] + j] = 0;
+      }
+    }
+
+    return newLines;
+  }
+
+  const countWithoutZero = (row: [number, number], col: [number, number]) : number => {
+    let cnt = 0;
+    for (let i = 0; i <= col[1]; i++) {
+      for (let j = 0; j <= row[1]; j++) {
+        if (numLines[col[0] + i][row[0] + j] !== 0) {
+          cnt++;
+        }
+      }
+    }
+
+    return cnt;
+  }
+
+  const calculateRectValue = (position: {start: {x: number, y: number}, end : {x: number, y:number}}): [number, [number, number], [number, number]] => {
+    let sum = 0;
+    let rs = position.start.x;
+    let re = position.end.x;
+    if (position.start.x > position.end.x) {
+      let tmp = re;
+      re = rs;
+      rs = tmp;
+    }
+
+    re = Math.floor((re - rs) / pixelWidth);
+    rs = Math.floor(rs / pixelWidth);
+
+    let cs = position.start.y;
+    let ce = position.end.y;
+    if (position.start.y > position.end.y) {
+      let tmp = ce;
+      ce = cs;
+      cs = tmp;
+    }
+    
+    ce = Math.floor((ce - cs) / pixelWidth);
+    cs = Math.floor(cs / pixelWidth);
+
+    for (let i = 0; i <= re && rs + i >= 0 && rs + i < 17; i++) {
+      for (let j = 0; j <= ce && cs + j >= 0 && cs + j < 10; j++) {
+        sum += numLines[cs + j][rs + i];
+      }
+    }
+
+    return [sum, [rs, re], [cs, ce]];
+  }
+
   useEffect(() => {
-    if (ref.current === null)
-    {
+    if (ref.current === null) {
       return ;
     }
 
     const ctx = ref.current.getContext("2d");
-    ctx?.scale(1, 1);
-    ref.current.width = 1700;
-    ref.current.height = 1000;
-    draw(ctx, line);
+    if (!ctx)
+      return ;
+
+    ctx.scale(1, 1);
+    ref.current.width = screenWidth;
+    ref.current.height = screenHeight;
+    ctx.font =  "60px Arial";
+   
+    draw(ctx, numLines);
   }, []);
 
   useEffect(() => {
@@ -60,36 +123,40 @@ export default function Home() {
       return ;
     
     ctx.clearRect(0, 0, ref.current.width, ref.current.height);
-    draw(ctx, line);
+    draw(ctx, numLines);
     if (position.start.x === -500 || position.end.x === -500)
-      return ;
-    
+      return ; 
     
     ctx.strokeStyle = "blue";
-    
     ctx.strokeRect(position.start.x, position.start.y, position.end.x - position.start.x, position.end.y - position.start.y);
-    ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
+    const [val] = calculateRectValue(position);
+    ctx.fillStyle= val === 10 ? "rgba(0, 0, 0, 0.5)": "rgba(0, 0, 0, 0.2)"
     ctx.fillRect(position.start.x, position.start.y, position.end.x - position.start.x, position.end.y - position.start.y);
-
     ctx.closePath();
-  }, [position]);
 
+  }, [position]);
+  
   return (
-    <div className = "flex mx-auto w-7/8 mt-10 bg-blue-200 flex-col" onMouseLeave={onDragEnd} onMouseUp={onDragEnd}>
+    <div className = "flex mx-auto w-7/8 mt-10 bg-blue-200 flex-col" onMouseLeave={onDragEnd} onMouseUp={() => {
+      const [val, row, col] = calculateRectValue(position);
+      if (val === 10) {
+        const count = countWithoutZero(row, col);
+        setLines(() => updateLine(row, col));
+        setValue((prev) => prev + count);
+      }
+
+      onDragEnd();
+    }}>
       <div className=" relative mx-auto bg-slate-500">
-        <canvas className="w-[1700px] h-[1000px] bg-red-500" ref={ref}>
+        <canvas className="w-[px] h-[1000px] bg-red-500 text-4xl" ref={ref}>
         </canvas>
         <div onMouseDown={onDragStart}
           onMouseMove={onDragDuration}
-          onMouseUp={onDragEnd}
           className = "w-[1700px] h-[1000px] top-0 left-0 absolute"
           >
         </div>
       </div>
-      x: {position.start.x}
-      y: {position.start.y}
-      w: {position.end.x}
-      h: {position.end.y}
+      value: {value}
     </div>
   );
 }
---
+++ src/constant/constant.tsx
@@ -0,0 +1,6 @@
+
+
+export const pixelWidth = 80;
+export const pixelHeight = 80;
+export const screenWidth = 100 * 17;
+export const screenHeight = 100 * 10;
